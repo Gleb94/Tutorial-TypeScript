24d42be6d2630efa9ae15f4d30f933117e1ff84a
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-11-03 17:50:48 +0300

    add

---
+++ typescript/1 types.js
@@ -0,0 +1,20 @@
+// var isFetching = true;
+// var isLoading = false;
+// var int = 42;
+// var float = 4.2;
+// var num = 3e10;
+// var message = 'Hello Typescript';
+// var numberArray = [1, 1, 2, 3, 8, 13];
+// var numberArray2 = [1, 1, 2, 3, 8, 13];
+// var words = ['Hello', 'Typescript'];
+// // Tuple
+// var contact = ['Vladelen', 1234567];
+// // Тип Any
+// var variable = 42;
+// //...
+// variable = 'New String';
+// variable = [];
+// // ====
+// function sayMyName(name) {
+//     console.log(name);
+// }
---
+++ typescript/1 types.ts
@@ -0,0 +1,28 @@
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = 'Hello Typescript';
+
+const numberArray: number[] = [1, 1, 2, 3, 8, 13];
+const numberArray2: Array<number> = [1, 1, 2, 3, 8, 13];
+
+const words: string[] = ['Hello', 'Typescript'];
+
+// Tuple
+const contact: [string, number] = ['Vladelen', 1234567];
+
+// Тип Any
+let variable: any = 42;
+//...
+variable = 'New String';
+variable = [];
+
+// ====
+// function sayMyName (name: string): void{
+//     console.log(name)
+// }
+// sayMyName(name:'Хайзенберг')
