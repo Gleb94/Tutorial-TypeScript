3eb8bb7484ee6d7019855ab9f7bc9305ff8ca7f1
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-11-07 17:16:23 +0300

    add

---
+++ typescript/3_enum.ts
@@ -0,0 +1,19 @@
+enum Membership{
+    Sipmpl,
+    Standard,
+    Premium
+}
+
+const membership = Membership.Standard;
+const membershipReverse = Membership[2];
+console.log(membership)
+console.log(membershipReverse)
+
+enum ocialMedia{
+    VK = 'VK',
+    FACEBOOK = 'FACEBOOK',
+    INSTAGRAM = 'INSTAGRAM'
+}
+
+const social = SocialMadia.INSTAGRAM
+console.log(social)
